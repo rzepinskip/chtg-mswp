368096ae5cbb5c49377d5a6aa534a6bb6884e08b
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-05-14 17:26:54 +0200

    Add coloring reconstruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import click
 from mswp.utils import get_sample_graph, draw_graph, draw_bipartite_graph
-from mswp.algo import MSWPAlgo
+from mswp.algo import MSWPAlgo, reconstruct_coloring
 
 
 @click.command()
@@ -8,6 +8,8 @@ def main():
     G = get_sample_graph()
     print(MSWPAlgo(G).mswp())
     draw_bipartite_graph(G)
+
+    print(reconstruct_coloring(G))
 
 
 if __name__ == "__main__":
